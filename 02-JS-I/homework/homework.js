a39c83e476f00fce8437e8e8db1d356ca5d1c404
pablo <pablondalpra@hotmail.com>
2022-03-09 08:55:52 -0300

    tarea1 parte3

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -69,12 +69,14 @@ function sonIguales(x, y) {
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
-  // Tu código: 
-
+  // Tu código:
+  return str1.length === str2.length; 
+}
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
+  return num <90
   
 }
 
@@ -82,6 +84,7 @@ function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
+  return num >50
   
 }
 
@@ -101,13 +104,14 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  return num % ==0
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
+  return ! esPar(num)
   
 }
 
